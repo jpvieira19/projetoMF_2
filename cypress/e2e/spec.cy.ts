75b012d49beb873008125adabf782c041093288c
mmartins <marcelo.martins@b-simple.pt>
2024-05-23 17:42:54 +0100

    cancel pipeline

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -29,52 +29,52 @@ describe('Project Management Application', () => {
     });
   });
 
-  /*it('Creates a new project and verifies count via filtering', () => {
-    cy.visit('http://localhost:4201/');
-    // Adiciona novo projeto
-    cy.contains('Create Project').click();
-    const randomNum = Math.floor(Math.random() * 10000);
-    const projeto = 'Projeto' + randomNum;
-    cy.wrap(projeto).as('newProjectName'); // Armazena o nome do projeto como alias
+  // it('Creates a new project and verifies count via filtering', () => {
+  //   cy.visit('http://localhost:4201/');
+  //   // Adiciona novo projeto
+  //   cy.contains('Create Project').click();
+  //   const randomNum = Math.floor(Math.random() * 10000);
+  //   const projeto = 'Projeto' + randomNum;
+  //   cy.wrap(projeto).as('newProjectName'); // Armazena o nome do projeto como alias
 
-    cy.get('input[name="Name"]').type(projeto);
-    cy.get('input[name="startDate"]').type('2025-05-01');
-    cy.get('input[name="endDate"]').type('2025-05-31');
-    cy.get('form').submit();
-    cy.wait(500); // Aguarde o projeto ser adicionado
+  //   cy.get('input[name="Name"]').type(projeto);
+  //   cy.get('input[name="startDate"]').type('2025-05-01');
+  //   cy.get('input[name="endDate"]').type('2025-05-31');
+  //   cy.get('form').submit();
+  //   cy.wait(500); // Aguarde o projeto ser adicionado
 
-    // Verifica se o projeto criado está na lista usando filtro
-    cy.get('input[placeholder="Filter"]').clear().type(projeto);
-    cy.wait(500); // Aguarde o filtro ocorrer
-    cy.get('tbody tr').should('have.length', 1);
-    cy.get('tbody tr').contains(projeto).parent().contains('Details').click();
-    cy.wait(500); // Aguarde os detalhes do projeto aparecerem
+  //   // Verifica se o projeto criado está na lista usando filtro
+  //   cy.get('input[placeholder="Filter"]').clear().type(projeto);
+  //   cy.wait(500); // Aguarde o filtro ocorrer
+  //   cy.get('tbody tr').should('have.length', 1);
+  //   cy.get('tbody tr').contains(projeto).parent().contains('Details').click();
+  //   cy.wait(500); // Aguarde os detalhes do projeto aparecerem
     
-  });
+  // });
 
-  it('Adds a new collaborator to the new project and verifies count via filtering', () => {
-    cy.get('@newProjectName').then((projectName) => {
+  // it('Adds a new collaborator to the new project and verifies count via filtering', () => {
+  //   cy.get('@newProjectName').then((projectName) => {
       
 
-      // Adiciona colaborador
-      cy.contains('Add Colaborator').click();
-      const randomColabNum = Math.floor(Math.random() * 10000);
-      const colaboradorId = randomColabNum.toString();
-      cy.wrap(colaboradorId).as('newColabId'); // Armazena o ID do colaborador como alias
+  //     // Adiciona colaborador
+  //     cy.contains('Add Colaborator').click();
+  //     const randomColabNum = Math.floor(Math.random() * 10000);
+  //     const colaboradorId = randomColabNum.toString();
+  //     cy.wrap(colaboradorId).as('newColabId'); // Armazena o ID do colaborador como alias
 
-      cy.get('select[name="colaboratorId"]').select('1');
-      cy.get('input[name="DataInicio"]').type('2025-05-01');
-      cy.get('input[name="DataFim"]').type('2025-05-31');
-      cy.get('form').submit();
-      cy.wait(500); // Aguarde a associação ser adicionada
+  //     cy.get('select[name="colaboratorId"]').select('1');
+  //     cy.get('input[name="DataInicio"]').type('2025-05-01');
+  //     cy.get('input[name="DataFim"]').type('2025-05-31');
+  //     cy.get('form').submit();
+  //     cy.wait(500); // Aguarde a associação ser adicionada
 
-      // Verifica se o colaborador criado está na lista usando filtro
-      cy.get('input[placeholder="Filter Colaborators"]').clear().type(colaboradorId);
-      cy.wait(500); // Aguarde o filtro ocorrer
-      cy.get('.project-details tbody tr').should('have.length', 1);
-      cy.get('.project-details tbody tr').contains(colaboradorId);
-    });
-  });*/
+  //     // Verifica se o colaborador criado está na lista usando filtro
+  //     cy.get('input[placeholder="Filter Colaborators"]').clear().type(colaboradorId);
+  //     cy.wait(500); // Aguarde o filtro ocorrer
+  //     cy.get('.project-details tbody tr').should('have.length', 1);
+  //     cy.get('.project-details tbody tr').contains(colaboradorId);
+  //   });
+  // });
 
   
 });
