a18dc8506e2d465cfacb8713288a2caac927ffb3
João Vieira <1201376@isep.ipp.pt>
2024-06-25 08:47:12 +0100

    trying to change version angular

--- projects/projeto/src/app/app.component.ts
+++ projects/projeto/src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { NgToastModule } from 'ng-angular-popup';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, CommonModule]
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NgToastModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'projeto';
+  title = 'Colaborator';
 }
